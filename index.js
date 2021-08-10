99f16430382a23403a6ed0867c6d852ec64a5136
Zeon <zeonstewart23@gmail.com>
2021-08-09 21:56:24 -0400

    commit tree 14d1dd36b524da0c911d773e0312821ca0224153

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+    // Write your code here!
+
+    if (rideDistance <= 400) {
+        return "This one is on me!";
+    } else if (rideDistance <= 2499) {
+        console.log(rideDistance);
+        return "I will gladly take your thirty bucks.";
+    } else if (rideDistance >= 2500) {
+        return "No can do.";
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+    // Write your code here!
+    return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amountLeft) {
+    // Write your code here!
+    switch (amountLeft) {
+        case "generous":
+            return "Thank you so much.";
+        case "not as generous":
+            return "Thank you.";
+        default:
+            return "Bye.";
+
+    }
+
 }
